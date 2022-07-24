ab61dec3e98288d82d28d2f1e59a244e42faddf7
Tom Fieber <thomasfieber@outlook.com>
2022-07-24 07:20:55 -0500

    Cleaned up functions, added banner parsing

--- nmapParse.py
+++ nmapParse.py
@@ -11,20 +11,24 @@ from termcolor import colored
 
 parser = argparse.ArgumentParser(description='Make constructing the IP table easy')
 parser.add_argument('-f', '--file', dest='file', type=str, required=True, help='The file to parse')
+parser.add_argument('-q', '--quiet', dest='quiet', action='store_true', help='Suppress the welcome banner and headlines')
 args = parser.parse_args()
 
 file = args.file
+quiet = args.quiet
 
 ips = {}
 listening = {}
 port_count = {}
+#port_info = {}
 
 # Print Welcome banner
-print("-" * 60)
-print("Simple Nmap Parser".center(60, " "))
-print("Version 1.0".center(60, " "))
-print("Author: Tom Fieber, Security Consultant".center(60, " "))
-print("-" * 60)        
+def greeting():
+    print("-" * 60)
+    print("Simple Nmap Parser".center(60, " "))
+    print("Version 1.0".center(60, " "))
+    print("Author: Tom Fieber, Security Consultant".center(60, " "))
+    print("-" * 60)        
 
 def populate_dictionaries():
     nmap_parse = NmapParser.parse_fromfile(file)
@@ -38,25 +42,60 @@ def populate_dictionaries():
             ips[ip] = [hostname]
         else:
             ips[ip].append(hostname)
+
+        # Get a dictionary object of all service details
         for service in host.services:
-            svcPort = str(service.port)
-            svcProt = str(service.protocol)
+            svcDetails = service.get_dict()
+            svcPort = svcDetails['port']
+
+            # Create the list of listening ports
             if ip not in listening.keys():
-                listening[ip] = {svcPort}
+                listening[ip] = [svcDetails]
             else:
-                listening[ip].add(svcPort)
+                listening[ip].append(svcDetails)
+
+            # Get a count of all ports across hosts
             if svcPort not in port_count.keys():
                 port_count[svcPort] = 1
             else:
                 port_count[svcPort] += 1
+
+def split_banner(b):
+    product = ""
+    version = ""
+    prodIndex = b.find('product')
+    versionIndex = b.find('version')
+    extraIndex = b.find('extrainfo')
+    if prodIndex != -1:
+        if versionIndex != -1:
+            product = b[prodIndex+9:versionIndex].strip()
+        else:
+            product = b[prodIndex+9:extraIndex].strip()
+    else:
+        product = 'Unknown'
+    if versionIndex != -1:
+        version = b[versionIndex+9:extraIndex].strip()
+    else:
+        version = 'Unknown'
+    return product, version
+
+def get_port_details(dict):
+    port = dict['port']
+    protocol = dict['protocol']
+    service = dict['service']
+    banner = dict['banner']
+    product, version = split_banner(banner)
+    print(colored(port, 'blue', attrs=['bold']), end=' ')
+    print(protocol, service, product, version)
 
 def get_hostnames(ip):
     for v in ips[ip]:
         print(colored(v, "green"))
 
 def print_dict():
-    print(colored("Use this section to generate tables of".center(60, " "), "grey", "on_yellow"))
-    print(colored("ports and services enumerated during testing".center(60, " "), "grey", "on_yellow"))
+    if not quiet:
+        print(colored("Use this section to generate tables of".center(60, " "), "grey", "on_yellow"))
+        print(colored("ports and services enumerated during testing".center(60, " "), "grey", "on_yellow"))
     for k in sorted(listening.keys()):
         print()
         print(colored(f"{k}", "yellow", attrs=['bold']))
@@ -65,19 +104,23 @@ def print_dict():
         get_hostnames(k)
         print()
         print(colored("---Open Ports---", "magenta"))
-        for v in sorted(listening[k], key=int):
-            print(colored(v, "blue"))
-        print()
+        for v in range(len(listening[k])):
+            get_port_details(listening[k][v])
         print()
 
 def count_open_ports():
-    print(colored("Use this section to generate the distribution".center(60, " "), "grey", "on_yellow"))
-    print(colored("chart of listening ports across all hosts".center(60, " "), "grey", "on_yellow"))
-    print()
+    if not quiet:
+        print(colored("Use this section to generate the distribution".center(60, " "), "grey", "on_yellow"))
+        print(colored("chart of listening ports across all hosts".center(60, " "), "grey", "on_yellow"))
+        print()
+    else:
+        print(colored("---Count of all listening ports---", "magenta"))
     for k in sorted(port_count.keys(), key=int):
         print(f"{k} : {port_count[k]}")
 
 if __name__ == '__main__':
+    if not quiet:
+        greeting()
     populate_dictionaries()
     print_dict()
     count_open_ports()
